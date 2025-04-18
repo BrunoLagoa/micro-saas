3e055e6bd836131ff7716de467842fd515f75278
Bruno Castro <miroldols@gmail.com>
2025-04-18 15:38:12 -0300

    feat: add Footer component and refactor page layouts

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { handleAuth } from '@/actions/handle-auth';
+import { Footer } from '@/components/footer/footer';
 import { auth } from '@/lib/auth';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
@@ -11,24 +12,29 @@ export default async function Dashboard() {
   }
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-10 text-4xl font-bold">Dashboard</h1>
-      <p>Email do usuário: {session?.user?.email}</p>
+    <div className="flex h-screen flex-col justify-center">
+      <div className="flex h-full flex-col items-center justify-center">
+        <h1 className="mb-10 text-4xl font-bold">Dashboard</h1>
+        <p>Email do usuário: {session?.user?.email}</p>
 
-      {session?.user?.email && (
-        <button
-          onClick={handleAuth}
-          className="mt-4 cursor-pointer rounded-md border bg-red-500 p-2 text-white transition-colors duration-300 ease-in-out hover:bg-red-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-        >
-          Logout
-        </button>
-      )}
+        {session?.user?.email && (
+          <button
+            onClick={handleAuth}
+            className="focus:ring-opacity-50 mt-4 cursor-pointer rounded-md border bg-red-500 p-2 text-white transition-colors duration-300 ease-in-out hover:bg-red-600 focus:ring focus:ring-blue-300 focus:outline-none"
+          >
+            Logout
+          </button>
+        )}
 
-      <Link href="/pagamentos" className="mt-4">
-        <button className="cursor-pointer rounded-md border bg-blue-500 p-2 text-white transition-colors duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-          Pagamentos
-        </button>
-      </Link>
+        <Link href="/pagamentos" className="mt-4">
+          <button className="focus:ring-opacity-50 cursor-pointer rounded-md border bg-blue-500 p-2 text-white transition-colors duration-300 ease-in-out hover:bg-blue-600 focus:ring focus:ring-blue-300 focus:outline-none">
+            Pagamentos
+          </button>
+        </Link>
+      </div>
+      <div className="bg-blue-100">
+        <Footer />
+      </div>
     </div>
   );
 }
